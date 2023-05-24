499e2c5542a0f8ebd87a51bcf916d7595aa0ba21
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-05-24 23:05:00 +0200

    1 video - add a new about route and improve home

--- flask_blog.py
+++ flask_blog.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  return '<h1>Hello Home<h1>'
+@app.route('/home')
+def home():
+  return '<h1>Home page</h1>'
+
+@app.route('/about')
+def about():
+  return '<h1>About page</h1>'
