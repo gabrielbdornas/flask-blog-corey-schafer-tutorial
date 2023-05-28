3392c16c1ecfbd78af5a557cdc684ae5c57c5dbe
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-05-28 19:39:15 +0200

    2 video - passing and using variables in loops and if

--- flask_blog.py
+++ flask_blog.py
@@ -5,11 +5,32 @@ from flask import (
 
 app = Flask(__name__)
 
+posts = [
+          {
+          'author': 'Gabriel Dornas',
+          'title': 'Blog Post 1',
+          'content': 'First post content.',
+          'date': 'May 28, 2023'
+          },
+          {
+          'author': 'Corey Shafer',
+          'title': 'Blog Post 2',
+          'content': 'Second post content.',
+          'date': 'May 29, 2023'
+          },
+          {
+          'author': 'Paul McCartney',
+          'title': 'Blog Post 3',
+          'content': 'Third post content.',
+          'date': 'May 30, 2023'
+          },
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('about.html', title='About')
