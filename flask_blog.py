fd2b19e3bd4b0835475600a298d0fe64c82502b1
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-06-12 14:31:30 +0200

    3 video - add forms with validation to register and login

--- flask_blog.py
+++ flask_blog.py
@@ -2,9 +2,15 @@ from flask import (
                    Flask,
                    render_template,
                    url_for,
+                   flash,
+                   )
+from forms import (
+                   RegistrationForm,
+                   LoginForm,
                    )
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '76ce9b71a7bb3360dfc5af8083732a8e'
 
 posts = [
           {
@@ -35,3 +41,22 @@ def home():
 @app.route('/about')
 def about():
   return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data.title()}!', 'success')
+    return redirect(url_for('home')) # home is the name of the function
+  return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+      flash('You have been logged in!', 'success')
+      return redirect(url_for('home'))
+    else:
+        flash('Login Unsuccessful. Please check username and password', 'danger')
+  return render_template('login.html', title='Login', form=form)
