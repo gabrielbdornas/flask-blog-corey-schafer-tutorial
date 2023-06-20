b627af9657f3cf3de16ad0b74434d449a90485a0
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-06-20 11:10:27 +0200

    4 video - create user(s) and post(s) models and tables

--- flask_blog.py
+++ flask_blog.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from flask import (
                    Flask,
                    render_template,
                    url_for,
                    flash,
                    )
+from flask_sqlalchemy import SQLAlchemy
 from forms import (
                    RegistrationForm,
                    LoginForm,
@@ -11,6 +13,30 @@ from forms import (
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '76ce9b71a7bb3360dfc5af8083732a8e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(60), nullable=False)
+  posts = db.relationship('Post', backref='author', lazy=True)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  # utcnow withou paranthesis because we want to pass the function as argument
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content = db.Column(db.Text, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+  def __repr__(self):
+    return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
           {
